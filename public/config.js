9affb955d882bc8e8b4e7a260b9bbc2422644ce9
admin-cmd <131160982+admincmd-a@users.noreply.github.com>
2024-06-10 08:52:39 +0800

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,36 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '网站监视器',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2576350-ed7055a4adb8e08d1ea0c1cb',
+    // 'm784490063-7b5da437e7f1e0d67613714d',
+    // 'm784497419-de55aa09902ccb3ab22d548a',
+    // 'm784496436-71a4bf7b1e3bdf7756be131b',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页 - GitHub',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: '主页 - Vercel',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: '主页 - Netlify',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
